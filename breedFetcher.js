b55f2ca37b2213e3a68e2a0b34f948f621e29c05
Stacey Keating <staceyamberkk@gmail.com>
2020-04-09 23:18:25 +0000

    breedFetcher fixed up and test added

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,12 +4,14 @@ const fetchBreedDescription = function(breedName, callback) {
   const serverAddress = `https://api.thecatapi.com/v1/breeds/search?q=` + breedName;
   request(serverAddress, (error, response, body) => {
     if (error) {
-      callback(`Error: ${error}`);
+      return callback(error);
     }
     
-    const data = JSON.parse(body);
-    if (data[0]) {
-      callback(data[0].description);
+    const desc = JSON.parse(body);
+    if (desc[0]) {
+      return callback(null, desc[0].description);
+    } else {
+    return callback(null);
     }
   });
 };
--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const breedName = process.argv[2];
 
 fetchBreedDescription(breedName, (error, desc) => {
   if (error) {
-    console.log(`Error fetch details:`, error);
+    console.log("Error fetch details:", error);
   } else {
-    console.log(desc);
+    console.log((desc));
   }
 });
